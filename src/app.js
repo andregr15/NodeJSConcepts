05173cc4c617db0ee49604708a7fa21ea8f58adb
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2020-06-23 08:55:14 -0300

    Finishing challenge

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +9,89 @@ app.use(cors());
 
 const repositories = [];
 
+function validateId(request, response, next) {
+  const { id } = request.params;
+
+  if(!isUuid(id)) {
+    return response.status(400).json({ error: 'Invalid ID!'});
+  }
+
+  return next();
+}
+
+app.use("/repositories/:id", validateId);
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repository);
+
+  response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repository = repositories.find(repository => repository.id === id);
+
+  if(!repository) {
+    return response.status(404).json({ error: `There is no repository with the id: ${id}`})
+  }
+
+  const { title, url, techs } = request.body;
+  if(title) {
+    repository.title = title;
+  }
+  if(url) {
+    repository.url = url;
+  }
+  if(techs) {
+    repository.techs = techs;
+  }
+
+  response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex < 0) {
+    return response.status(404).json({ error: `There is no repository with the id: ${id}`})
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repository = repositories.find(repository => repository.id === id);
+
+  if(!repository) {
+    return response.status(404).json({ error: `There is no repository with the id: ${id}`})
+  }
+
+  repository.likes++;
+  response.json({
+    id: repository.id,
+    likes: repository.likes
+  });
 });
 
 module.exports = app;
